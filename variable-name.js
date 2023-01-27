6184d95217cfa30dd4c96e34944130774ee84383
sabbir1257 <masabbir1257@gmail.com>
2023-01-27 11:54:19 +0600

    first commit

---
+++ variable-name.js
@@ -0,0 +1,30 @@
+// good variable
+var price = 29;
+
+// 1.variable name can not be any keywords.
+var false = 29;
+var return = true;
+
+// 2.variable name has to be in one work, no space
+var my home addeas = "jonota houging" ;
+
+// 3.variable name can not have quotation
+var "name" = "sabbir" ;
+
+// 4. variable name can not starts with a number but can ends with a number
+var 1996club = 1996;
+var club1996 = 1996;
+
+// How to use long names
+// can not use dash
+var user-name ="sabbir sarker";
+var user_name = "sabbir sarker";
+var userhoumaddes = "mirpur jonota hougin";  //snake case
+var userHomeAddeas = "mirpur jonota hougin"  //camel case:  we will use this one 
+var UserHomeAddeas = "mirpur jonota hougin"  //pascal case
+
+
+// variable name is case senstive 
+
+var price = 23;
+var Price =25;
---
+++ variable.js
@@ -0,0 +1,16 @@
+// vary + able = varyable
+
+// numeric
+var price = 11;
+var age = 23;
+var temperature = 37;
+
+// string
+var parson = "sabbir sarker" ;
+var location = "jonota hougin" ;
+var spceial = "alia bhatt" ;
+
+// Booiean
+var serious = true ;
+var isfullMarks = true ;
+var issingle = false ;
