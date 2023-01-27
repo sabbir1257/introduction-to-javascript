d64ed3d6618d949a513743779286fc126e383381
sabbir1257 <masabbir1257@gmail.com>
2023-01-27 14:51:41 +0600

    push

---
+++ operation.js
@@ -0,0 +1,28 @@
+var onionPrice = 20;
+var eggPrice = 32;
+
+// addition, subtraction
+var totalPrice = onionPrice + eggPrice ;
+var priceDifference = onionPrice - eggPrice ;
+
+// console.log(priceDifference);
+
+// console.log(totalPrice);
+
+// console.log(onionPrice);
+// console.log(eggPrice)
+// console.log(onionPrice + eggPrice)
+
+
+// multplication
+var orangePrice = 30;
+var quantiy = 7;
+var totalCost = orangePrice * quantiy;
+// console.log(totalCost) 
+
+
+// division
+var many = 500;
+var player =10;
+var eachPlayer = many / player;
+console.log(eachPlayer)
---
+++ shorthand.js
@@ -0,0 +1,14 @@
+// variable declaration
+var tomatoPrice =25;
+// variable valu update
+tomatoPrice =30;
+
+
+// add samething to the previous value
+var price1 =30;
+var price2 =20;
+
+// set a new value
+
+
+// add some value to tha existing value
