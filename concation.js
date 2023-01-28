248f2c43afe45df010798f08b9d0c04965ff2f1a
sabbir1257 <masabbir1257@gmail.com>
2023-01-28 10:40:57 +0600

    push

---
+++ concation.js
@@ -0,0 +1,8 @@
+var price1 = 31;
+var price2 = 10;
+var tomatoPrice = price1 + price2 ;
+// console.log(tomatoPrice); 
+var firstName = 'Sabbir';
+var lastName = 'Sarker';
+var fullName = firstName + ' ' + lastName;
+console.log(fullName);
--- shorthand.js
+++ shorthand.js
@@ -9,6 +9,13 @@ var price1 =30;
 var price2 =20;
 
 // set a new value
-
+// price1 = 40;
 
 // add some value to tha existing value
+price1 = price1 + 10;
+console.log(price1);
+
+
+// addition to tha same variable shorthand
+// += means adding some value to the existing value
+price1 += 10;
